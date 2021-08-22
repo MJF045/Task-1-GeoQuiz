5b77be999a3cf6af06d28fb2a04dc35e358abe92
MJF045 <82356633+MJF045@users.noreply.github.com>
2021-08-22 20:53:11 +1000

    Add files via upload

---
+++ GeoQuiz/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.kt
@@ -0,0 +1,75 @@
+package com.bignerdranch.android.geoquiz
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewAnimationUtils
+import android.widget.Button
+import android.widget.TextView
+import com.bignerdranch.android.geoquiz.IntentExtraKeyConstants.EXTRA_ANSWER_IS_TRUE
+import com.bignerdranch.android.geoquiz.IntentExtraKeyConstants.EXTRA_ANSWER_SHOWN
+
+class CheatActivity : AppCompatActivity() {
+
+    private var mAnswerIsTrue: Boolean = false
+
+    private lateinit var mAnswerTextView: TextView
+    private lateinit var mShowAnswerButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cheat)
+
+        mAnswerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+
+        mAnswerTextView = findViewById(R.id.answer_text_view)
+
+        mShowAnswerButton = findViewById(R.id.show_answer_button)
+        mShowAnswerButton.setOnClickListener({
+            mAnswerTextView.setText(
+                    if (mAnswerIsTrue) R.string.true_button
+                    else R.string.false_button
+            )
+            setAnswerShownResult(true)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                val cx: Int = mShowAnswerButton.width / 2
+                val cy: Int = mShowAnswerButton.height / 2
+                val radius: Float = mShowAnswerButton.width.toFloat()
+                var anim: Animator = ViewAnimationUtils.createCircularReveal(mShowAnswerButton, cx, cy, radius, 0.toFloat())
+                anim.addListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator?) {
+                        super.onAnimationEnd(animation)
+                        mShowAnswerButton.visibility = View.INVISIBLE
+                    }
+                })
+                anim.start()
+            } else {
+                mShowAnswerButton.visibility = View.INVISIBLE
+            }
+        })
+    }
+
+    private fun setAnswerShownResult(isAnswerShown: Boolean) {
+        val data = Intent().putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown)
+        setResult(Activity.RESULT_OK, data)
+    }
+
+    companion object {
+        fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
+            val intent = Intent(packageContext, CheatActivity::class.java)
+            intent.putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+            return intent
+        }
+
+        fun wasAnswerShown(result: Intent): Boolean {
+            return result.getBooleanExtra(EXTRA_ANSWER_SHOWN, false)
+        }
+    }
+}
---
+++ GeoQuiz/GeoQuiz/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.kt
@@ -0,0 +1,133 @@
+package com.bignerdranch.android.geoquiz
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class QuizActivity : AppCompatActivity() {
+
+    private val TAG: String = "QuizActivity"
+    private val KEY_INDEX: String = "index"
+    private val REQUEST_CODE_CHEAT: Int = 0
+
+    private lateinit var mTrueButton: Button
+    private lateinit var mFalseButton: Button
+    private lateinit var mNextButton: Button
+    private lateinit var mCheatButton: Button
+    private lateinit var mQuestionTextView: TextView
+
+    private var mCurrentIndex: Int = 0
+    private var mIsCheater: Boolean = false
+
+    private var mQuestionBank = arrayOf(
+            Question(R.string.question_australia, true),
+            Question(R.string.question_oceans, true),
+            Question(R.string.question_mideast, false),
+            Question(R.string.question_africa, false),
+            Question(R.string.question_americas, true),
+            Question(R.string.question_asia, true)
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle) called")
+        setContentView(R.layout.activity_quiz)
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX)
+        }
+
+        mQuestionTextView = findViewById(R.id.question_text_view)
+        updateQuestion()
+
+        mTrueButton = findViewById(R.id.true_button)
+        mTrueButton.setOnClickListener({
+            checkAnswer(true)
+        })
+
+        mFalseButton = findViewById(R.id.false_button)
+        mFalseButton.setOnClickListener({
+            checkAnswer(false)
+        })
+
+        mNextButton = findViewById(R.id.next_button)
+        mNextButton.setOnClickListener({
+            mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.size
+            updateQuestion()
+        })
+
+        mCheatButton = findViewById(R.id.cheat_button)
+        mCheatButton.setOnClickListener({
+            val answerIsTrue: Boolean = mQuestionBank[mCurrentIndex].mAnswerTrue
+            val intent = CheatActivity.newIntent(this@QuizActivity, answerIsTrue)
+            startActivityForResult(intent, REQUEST_CODE_CHEAT)
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode != Activity.RESULT_OK) return
+
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            if (data == null) return
+            mIsCheater = CheatActivity.wasAnswerShown(data)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        Log.i(TAG, "onSaveInstanceState")
+        outState?.putInt(KEY_INDEX, mCurrentIndex)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
+    private fun updateQuestion() {
+        val question: Int = mQuestionBank[mCurrentIndex].mTextResId
+        mQuestionTextView.text = getString(question)
+    }
+
+    private fun checkAnswer(userPressedTrue: Boolean) {
+        val answerIsTrue: Boolean = mQuestionBank[mCurrentIndex].mAnswerTrue
+
+        val messageResId: Int =
+                when {
+                    // Checks if user has cheated before checking if their answer is correct
+                    mIsCheater -> R.string.judgment_toast
+                    userPressedTrue == answerIsTrue -> R.string.correct_toast
+                    else -> R.string.incorrect_toast
+                }
+
+        Toast.makeText(this@QuizActivity,
+                messageResId,
+                Toast.LENGTH_SHORT).show()
+    }
+
+}
